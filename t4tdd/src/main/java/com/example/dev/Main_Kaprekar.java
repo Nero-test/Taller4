94b78660c1a670a1d96eab22ff105c558f9b8501
E.Cancino <116973712+Nero-test@users.noreply.github.com>
2024-11-15 11:26:24 -0300

    Add files via upload

--- t4tdd/src/main/java/com/example/dev/Main_Kaprekar.java
+++ t4tdd/src/main/java/com/example/dev/Main_Kaprekar.java
@@ -10,9 +10,23 @@ public class Main_Kaprekar {
 
 public static void main(String[] args){
     System.out.println("Clase Main");
+    System.out.println(kaprekarOp(3524));
 
 }
 
 //aquí van sus funciones:
+
+    public static int kaprekarOp(int numero){
+
+        int numero_ordenado_mayor=5432;
+        int numero_orenado_menor=2345;
+        int resta = numero_ordenado_mayor-numero_orenado_menor;
+        return resta;
+
+    }
+
+    public static int itKaprekar(int num){
+    return 0;
+    }
 
 }
